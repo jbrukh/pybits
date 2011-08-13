f73bd7bec618b26eab6284933170cc50b11c8912
Jake Brukhman <jbrukh@gmail.com>
2011-08-13 19:44:35 -0400

    Save.

--- subsort.py
+++ subsort.py
@@ -4,26 +4,26 @@ import re
 import sys
 
 class SubComparable(object):
-	
-	def __init__(self, line, pattern):
-		self.line = line
-		if pattern:
-			s = re.search(pattern, line)
-			if s:
-				self.substr = s.group(0)
-			else:
-				self.substr = line
+    """
+    Comparator that orders strings by the lexicographical
+    order of a subregex.
+    """
+    def __init__(self, line, pattern):
+        self.line = line
+        self.substr = None
+        if pattern:
+            s = re.search(pattern, line)
+            if s:
+                self.substr = s.group(0)
 
-	def __lt__(self,other):
+    def __lt__(self, other):
+		if not self.substr:
+			return self.line.__lt__(other.line)
 		return self.substr.__lt__(other.substr)
 
-	def __repr__(self):
-		return "'%s'"%self.line
-	
-	def __str__(self):
-		return self.substr
-
-
+    def __repr__(self):
+        return self.line
+    
 pattern = None
 if len(sys.argv) > 1:
 	pattern = sys.argv[1]
@@ -32,7 +32,6 @@ h = []
 for line in sys.stdin.readlines():
 	subline = SubComparable(line.strip(), pattern)
 	heapq.heappush(h,subline)
-	print h
 
 for i in range(len(h)):
 	print heapq.heappop(h)
